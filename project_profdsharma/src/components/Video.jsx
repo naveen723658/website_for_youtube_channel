e37f6921eb08aa6a4dbad7d452b89303fb312579
Naveen Kumar <naveen.kumar723658@gmail.com>
2023-02-09 04:05:58 +0530

    created more components modified Video Detail page

--- project_profdsharma/src/components/Video.jsx
+++ project_profdsharma/src/components/Video.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import react, { useState, useEffect } from "react";
+import react, { useState, useEffect, useRef } from "react";
 import HtmlToReact from "html-to-react";
 import { FaAngleRight, FaUser, FaBook } from "react-icons/fa";
 import axios from "axios";
@@ -7,52 +7,61 @@ import { useParams, Link } from "react-router-dom";
 import Topheader from "./Topheader";
 
 const Video = () => {
-  const [Playlist, setPlaylist] = useState([]);
   const parser = new HtmlToReact.Parser();
 
   const parm = useParams();
   const VIDEO_ID = parm.videoId;
-  const playlistId = parm.playlistId;
+  const [Playlist, setPlaylist] = useState(null);
   const [playlistData, setPlaylistData] = useState([]);
   const [videodetail, setvideodetail] = useState([]);
   const [commentdata, setcommentdata] = useState([]);
+
+  const textareaRef = useRef(null);
+  const [backUp, setBackUp] = useState("Add Your Comment");
+  const [showBtn, setShowBtn] = useState(false);
+
+  const handleFocus = () => {
+    setBackUp("");
+    textareaRef.current.style.borderColor = "#333";
+    setShowBtn(true);
+  };
+
+  const handleBlur = () => {
+    setBackUp("Add Your Comment");
+    textareaRef.current.style.borderColor = "#aaa";
+  };
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    setShowBtn(false);
+    textareaRef.current.value = "";
+  };
+
   const fetchData = async () => {
     try {
-      // video details or single video
+      // Videos Details
       const videodetails = await axios.get(
         `http://127.0.0.1:8000/video_detail/${VIDEO_ID}`
       );
-      console.log(videodetails.data.items);
+      console.log(videodetails.data);
       setvideodetail(videodetails.data.items);
-
-      // PlayList Item
-      const playlists = await axios.get("http://127.0.0.1:8000/playlists/");
-      setPlaylist(playlists.data.items);
-
-      // PlayList Video
+        // Playlist Items
       const playlistVideos = await axios.get(
-        `http://127.0.0.1:8000/playlist_items/${playlistId}/8`
+        `http://127.0.0.1:8000/playlistsitem/${VIDEO_ID}`
       );
-      setPlaylistData(
-        playlistVideos.data.items.filter(
-          (video) => video.snippet.resourceId.videoId !== VIDEO_ID
-        )
-      );
+      setPlaylistData(playlistVideos.data.items);
 
       // comments
       const comments = await axios.get(
-        `http://127.0.0.1:8000/comments/${VIDEO_ID}`
+        `http://127.0.0.1:8000/comments/${VIDEO_ID}/`
       );
-      const commentsList = comments.data.items;
+      setcommentdata(comments.data);
 
-      // Iterate over comments list to fetch comments and replies
-      for (const comment of commentsList) {
-        const replies = await axios.get(
-          `http://127.0.0.1:8000/comments_replies/${comment.id}`
-        );
-        comment.replies = replies.data.items;
-      }
-      setcommentdata(commentsList);
+      // Playlist
+      const playlist = await axios.get(
+        `http://127.0.0.1:8000/playlists/`
+      );
+      setPlaylist(playlist.data);
     } catch (error) {
       console.log(error);
     }
@@ -78,194 +87,174 @@ const Video = () => {
     <>
       {videodetail[0] && (
         <>
-
-          <Topheader title={videodetail[0].snippet.title} subtitle="About"/>
+          <Topheader title={videodetail[0].snippet.title} subtitle="About" />
           {/* Header section end */}
 
-          <div className="container-fluid video-blog">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8">
-                  <div className="video-cover">
-                    <iframe
-                      id="video"
-                      height={415}
-                      src={`https://www.youtube.com/embed/${videodetail[0].id}`}
-                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    />
-                    <div className="bg-light p-4 discr">
-                      {parser.parse(
-                        convertToHtml(
-                          videodetail[0] && videodetail[0].snippet.description
-                        )
-                      )}
-                    </div>
-                    <div className="w-100 my-4">
-                      <h5 className="d-flex align-item-center ">
-                        <FaBook />{" "}
-                        <div className="mx-2">
-                          {videodetail[0].statistics.commentCount} Comments
-                        </div>
-                      </h5>
-                    </div>
-
-                    <div className="comment-container my-4">
-                      {commentdata &&
-                        commentdata.map((data, index) => (
-                          <>
-                            <div className="comment-box row mx-0" key={index}>
-                              <div className="col-2 img">
-                                {data.snippet.topLevelComment.snippet
-                                  .authorProfileImageUrl && (
-                                  <img
-                                    src={
-                                      data.snippet.topLevelComment.snippet
-                                        .authorProfileImageUrl
-                                    }
-                                    alt=""
-                                  />
-                                )}
-                                {!data.snippet.topLevelComment.snippet
-                                  .authorProfileImageUrl && (
-                                  <span>
-                                    <FaUser />
-                                  </span>
-                                )}
-                              </div>
-                              <div className="col-10 detaila">
-                                <h6>
-                                  {
-                                    data.snippet.topLevelComment.snippet
-                                      .authorDisplayName
-                                  }
-                                  <small className="mx-2">
-                                    <span>posted on:</span>
-                                    {data.snippet.topLevelComment.snippet.updatedAt.slice(
-                                      0,
-                                      10
-                                    )}
-                                  </small>
-                                </h6>
-                                <p>
-                                  {
-                                    data.snippet.topLevelComment.snippet
-                                      .textDisplay
-                                  }
-                                </p>
-                              </div>
-                            </div>
-                          </>
-                        ))}
-
-                      <div className="d-flex justify-content-end align-item-center">
-                        <button className="btn btn-danger">Load more</button>
-                      </div>
-                    </div>
-
-                    <div className="w-100 my-4 pt-4">
-                      <h5 className="d-flex align-item-center ">
-                        <FaBook /> <div className="mx-2">Post Your Comment</div>
-                      </h5>
-                    </div>
-
-                    <div className="comment-text mx-0">
-                      <div className="form-row mx-0 row">
-                        <input
-                          type="text"
-                          placeholder=" Enter Name"
-                          className="form-control mx-0 form-control-sm"
-                        />
-                      </div>
-                      <div className="form-row mx-0 row">
-                        <input
-                          type="text"
-                          placeholder="Enter Mobile number"
-                          className="form-control form-control-sm"
-                        />
-                      </div>
-                      <div className="form-row mx-0 row">
-                        <input
-                          type="text"
-                          placeholder="Enter Email Address"
-                          className="form-control form-control-sm"
-                        />
-                      </div>
-                      <div className="form-row mx-0 row">
-                        <textarea
-                          placeholder="Enter Comment"
-                          rows={5}
-                          className="form-control form-control-sm"
-                          defaultValue={""}
-                        />
-                      </div>
-                      <div className="form-row row">
-                        <button className="btn btn-danger">Post Comment</button>
-                      </div>
-                    </div>
+          <div className="container" id="detail">
+            <div className="main-video-container">
+              <iframe
+                className="main-video"
+                height={415}
+                src={`https://www.youtube.com/embed/${videodetail[0].id}`}
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+              <div className="w-100 my-4">
+                <h5 className="d-flex align-item-center ">
+                  <FaBook />{" "}
+                  <div className="mx-2">
+                    {videodetail[0].statistics.commentCount} Comments
                   </div>
-                </div>
-
-                <div className="col-md-4">
-                  <div className="row no-margin video-title">
-                    <h5>
-                      <FaBook /> Related Videos
+                </h5>
+              </div>
+              {/* <h3 className="main-vid-title">house flood animation</h3> */}
+              <div className="bg-light p-4 discr">
+                {parser.parse(
+                  convertToHtml(
+                    videodetail[0] && videodetail[0].snippet.description
+                  )
+                )}
+              </div>
+              <div className="comments my-4 py-2 ">
+                <form>
+                  <div className="w-100 my-4 py-2">
+                    <h5 className="d-flex align-item-center ">
+                      <FaBook /> <div className="mx-2">Post Your Comment</div>
                     </h5>
                   </div>
-                  {playlistData &&
-                    playlistData.map((data, index) => (
-                      <Link
-                        to={`/about/${data.snippet.resourceId.videoId}/${data.snippet.playlistId}`}
+                  <textarea
+                    ref={textareaRef}
+                    placeholder={backUp}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                  />
+                  {showBtn && (
+                    <div className="btn">
+                      <input
+                        className="btn btn-sm btn-primary mr-2"
+                        type="submit"
+                        defaultValue="Comment"
+                      />
+                      <button
+                        className="btn btn-sm btn-warning"
+                        id="clear"
+                        href="#"
+                        onClick={handleClear}
                       >
-                        <div
-                          className="contri-bghy"
-                          key={index}
-                          id={data.snippet.publishedAt}
-                        >
-                          <div className="image">
-                            <img
-                              className="rlv"
-                              src={data.snippet.thumbnails.medium.url}
-                              alt=""
-                            />
-                          </div>
-                          <div className="detail">
-                            <h6>{data.snippet.title.slice(0, 30)} ...</h6>
+                        Cancel
+                      </button>
+                    </div>
+                  )}
+                </form>
+              </div>
+              <div className="comment-container my-4">
+                {commentdata.items &&
+                  commentdata.items.map((data, index) => (
+                    <>
+                      <div className="comment-box row mx-0" key={index}>
+                        <div className="col-2 img">
+                          {data.authorProfileImageUrl && (
+                            <img src={data.authorProfileImageUrl} alt="" />
+                          )}
+                          {!data.authorProfileImageUrl && (
                             <span>
-                              Posted on: {data.snippet.publishedAt.slice(0, 10)}
+                              <FaUser />
                             </span>
-                          </div>
+                          )}
                         </div>
-                      </Link>
-                    ))}
-                  {/* <div
-                    className="row no-margin video-title"
-                    bis_skin_checked={1}
-                  >
-                    <h6>
-                      <i className="fas fa-book" /> Our Top Cetegory
-                    </h6>
-                  </div> */}
-
-                  {/* {Playlist &&
-                    Playlist.map((item, index) => (
-                      <div className="contri-row" key={index}>
-                        <div className="image">
-                          <img
-                            src={item.snippet.thumbnails.medium.url}
-                            alt=""
-                          />
-                        </div>
-                        <div className="detail">
-                          <h6>{item.snippet.title}</h6>
-                          <span>
-                            Published on: {item.snippet.publishedAt.slice(0, 4)}
-                          </span>
-                          <p>{item.contentDetails.itemCount} Videos</p>
+                        <div className="col-10 detaila">
+                          <h6>
+                            {data.author}
+                            <small className="mx-2">
+                              <span>posted on:</span>
+                              {data.updatedAt.slice(0, 10)}
+                            </small>
+                          </h6>
+                          <p>{data.text}</p>
                         </div>
                       </div>
-                    ))} */}
+                    </>
+                  ))}
+
+                <div className="d-flex py-4 my-2 justify-content-end align-item-center">
+                  <a
+                    href={`https://www.youtube.com/watch?v=${VIDEO_ID}`}
+                    className="btn btn-danger"
+                    target="_blank"
+                  >
+                    View More
+                  </a>
                 </div>
+              </div>
+            </div>
+            <div className=" video-list-main-div">
+              <div className="mb-4 video-list-inner-div">
+              <div className="row no-margin video-title">
+                <h5>
+                  <FaBook /> Related Videos
+                </h5>
+              </div>
+              <div className="video-list-container">
+                <Link to={`/about/${VIDEO_ID}`} className={`list active`}>
+                  {/* <video src="images/vid-1.mp4" className="list-video" /> */}
+                  <img
+                    className="list-video"
+                    src={videodetail[0].snippet.thumbnails.medium.url}
+                    alt=""
+                  />
+
+                  <h3 className="list-title">
+                    {videodetail[0].snippet.title}...
+                  </h3>
+                </Link>
+
+                {playlistData &&
+                  playlistData.map((data, index) => (
+                    <>
+                      <Link
+                        to={`/about/${data.id.videoId}`}
+                        className={`list`}
+                        key={index}
+                      >
+                        {/* <video src="images/vid-1.mp4" className="list-video" /> */}
+                        <img
+                          className="list-video"
+                          src={data.snippet.thumbnails.medium.url}
+                          alt=""
+                        />
+
+                        <h3 className="list-title">
+                          {data.snippet.title.slice(0, 50)}...
+                        </h3>
+                      </Link>
+                    </>
+                  ))}
+              </div>
+              </div>
+
+
+              <div className="my-4  video-list-inner-div">
+              <div className="row no-margin video-title">
+                <h5>
+                  <FaBook /> Top Rated Playlist
+                </h5>
+              </div>
+              <div className="video-list-container">
+                {Playlist &&
+                  Playlist.items.map((data, index) => (
+                    <>
+                      <Link
+                        to={`/about/${VIDEO_ID}`}
+                        className={`list`}
+                        key={index}
+                      >
+                        <h3 className="list-title">
+                          {data.snippet.title}
+                        </h3>
+                      </Link>
+                    </>
+                  ))}
+              </div>
               </div>
             </div>
           </div>
