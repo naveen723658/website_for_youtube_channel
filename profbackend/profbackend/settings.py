a90efbf99b83c7dd81546da41ff2a697ae07e409
Naveen Kumar <naveen.kumar723658@gmail.com>
2024-05-12 19:56:50 +0530

    collectstatics

--- profbackend/profbackend/settings.py
+++ profbackend/profbackend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-STATIC_ROOT = [BASE_DIR / "staticfiles_build" , "static"]
+STATIC_ROOT = BASE_DIR / "static"
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
