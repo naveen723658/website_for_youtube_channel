008cbe7f955a84e0d2f80984c3b0646c642adf3c
Naveen Kumar <naveen.kumar723658@gmail.com>
2024-05-12 15:27:49 +0530

    vercel changes

--- profbackend/profbackend/settings.py
+++ profbackend/profbackend/settings.py
@@ -137,13 +137,13 @@ AWS_S3_FILE_OVERWRITE = False
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-STATIC_ROOT = BASE_DIR / "static"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATIC_ROOT = BASE_DIR / "staticfiles_build"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 
